b664de623cf920eef43158adc67a204f30c81104
Filipe Garrote <filipe.garrote@gmail.com>
2022-09-29 12:21:54 -0300

    feat(server) - Criação de servidor express

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import cors from 'cors';
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+server.listen(process.env.PORT, () =>
+  console.log(`Listening on port ${process.env.PORT}`)
+);
